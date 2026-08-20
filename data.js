7ffe1cb18cf40a8b77569cdd8c9d36b44e9d6527
sarkaricareerindia <praveenshabana646@gmail.com>
2026-08-20 07:58:04 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,32 @@
+const jobs = [
+  {
+    title: "BPSC 2026 – 67th Combined Pre Exam",
+    category: "Bihar",
+    type: "Government Job",
+    date: "09 Aug 2026"
+  },
+  {
+    title: "SSC CGL 2026",
+    category: "Central",
+    type: "Government Job",
+    date: "08 Aug 2026"
+  },
+  {
+    title: "UPSC Civil Services 2026",
+    category: "Central",
+    type: "Government Job",
+    date: "08 Aug 2026"
+  },
+  {
+    title: "Bihar Police Constable 2026",
+    category: "Bihar",
+    type: "Police Job",
+    date: "07 Aug 2026"
+  },
+  {
+    title: "RRB Group D 2026",
+    category: "Railway",
+    type: "Railway Job",
+    date: "07 Aug 2026"
+  }
+];
